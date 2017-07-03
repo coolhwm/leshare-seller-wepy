8489285d4370b9742c24a661ba0de9013786bb0f
coolhwm <coolhwm@qq.com>
2017-07-03 19:42:05 +0800

    清理测试文件，完成发货表单

--- src/api/order.js
+++ src/api/order.js
@@ -90,7 +90,7 @@ export default class order extends base {
     this._processOrderPrice(detail);
     // 处理商品信息
     this._processOrderGoods(detail.orderGoodsInfos);
-
+    return detail;
   }
 
   /**
