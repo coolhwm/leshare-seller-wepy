813753936554b2881a0c31fa531c4693d31ac3d7
coolhwm <coolhwm@qq.com>
2017-07-09 18:17:37 +0800

    商品编辑页面70%

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,5 +1,22 @@
 import base from './base';
+import wepy from 'wepy';
 
 export default class goods extends base {
+
+  /**
+   * 上传图片
+   */
+  static async image(filePath) {
+    const url = `${this.baseUrl}/images`;
+    const param = {
+      url,
+      filePath,
+      name: 'image'
+    }
+    const result = await wepy.uploadFile(param);
+    console.info(result);
+    return result;
+  }
+
   static create(goods) {}
 }
