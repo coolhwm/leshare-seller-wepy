bd308fde4180354b68768d7c9de8e145fe58c0f6
coolhwm <coolhwm@qq.com>
2017-07-09 22:45:19 +0800

    完成商品分类组件

--- src/api/base.js
+++ src/api/base.js
@@ -5,4 +5,5 @@ export default class base {
   static baseUrl = wepy.$instance.globalData.baseUrl;
   static get = http.get.bind(http);
   static put = http.put.bind(http);
+  static post = http.post.bind(http);
 }
--- src/api/goods.js
+++ src/api/goods.js
@@ -4,19 +4,44 @@ import wepy from 'wepy';
 export default class goods extends base {
 
   /**
+   * 商品分类
+   */
+  static async getInnerCategories() {
+    const url = `${this.baseUrl}/goods/inner_category`;
+    return await this.get(url);
+  }
+
+  /**
+   *  新增商品分类
+   */
+  static async addInnerCategories(name) {
+    const url = `${this.baseUrl}/goods/inner_category`;
+    const param = {
+      name: name,
+      pid: 0,
+      seq: 0,
+    }
+    return await this.post(url, param);
+  }
+
+  /**
    * 上传图片
    */
   static async image(filePath) {
-    const url = `${this.baseUrl}/images`;
+    // const url = `${this.baseUrl}/images`;
+    const url = `http://115.28.93.210:9999/api/v1/seller/images`;
     const param = {
       url,
       filePath,
       name: 'image'
     }
-    const result = await wepy.uploadFile(param);
-    console.info(result);
-    return result;
+    return await wepy.uploadFile(param);
   }
-
-  static create(goods) {}
+  /**
+   * 创建商品
+   */
+  static async create(goods) {
+    const url = `${this.baseUrl}/goods`;
+    return await this.post(url, goods);
+  }
 }
