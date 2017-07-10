9dc87314bd23a8c995a3a57c674bf9664f4d8d08
coolhwm <coolhwm@qq.com>
2017-07-10 12:40:17 +0800

    完成商品详情页面

--- src/utils/Event.js
+++ src/utils/Event.js
@@ -3,6 +3,8 @@ const WxNotificationCenter = require('./WxNotificationCenter.js');
 export default class Event {
   // 订单列表更新事件
   static ORDER_LIST_UPDATE = 'ORDER_LIST_UPDATE';
+  // 商品详情更新事件
+  static GOODS_DETAILS_UPDATE = 'GOODS_DETAILS_UPDATE';
   static listen(eventName, callback, observer) {
     WxNotificationCenter.addNotification(eventName, callback, observer);
   }
