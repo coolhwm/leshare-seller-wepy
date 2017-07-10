3adc0e5b61bdf6a92a71e66c8880854e7470a5dc
coolhwm <coolhwm@qq.com>
2017-07-10 21:08:33 +0800

    完成订单列表样式

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,8 +1,16 @@
 import base from './base';
 import wepy from 'wepy';
+import Page from '../utils/Page';
 
 export default class goods extends base {
 
+  /**
+   * 分页方法
+   */
+  static page() {
+    const url = `${this.baseUrl}/goods`;
+    return new Page(url, this._processGoodsListItem.bind(this));
+  }
   /**
    * 商品分类
    */
@@ -19,7 +27,7 @@ export default class goods extends base {
     const param = {
       name: name,
       pid: 0,
-      seq: 0,
+      seq: 0
     }
     return await this.post(url, param);
   }
@@ -43,5 +51,69 @@ export default class goods extends base {
   static async create(goods) {
     const url = `${this.baseUrl}/goods`;
     return await this.post(url, goods);
+  }
+
+  /**
+   * 处理商品列表数据
+   */
+  static _processGoodsListItem(goods) {
+    this._processGoodsPreview(goods);
+    this._processGoodsPriceRange(goods);
+    this._processGoodsSkuCount(goods);
+  }
+
+  /**
+   * 处理SKU数量
+   */
+  static _processGoodsSkuCount(item) {
+    if (!item.goodsSkuInfo || !item.goodsSkuInfo.goodsSkuDetails) {
+      item.skuCount = 0;
+    } else {
+      item.skuCount = item.goodsSkuInfo.goodsSkuDetails.length;
+    }
+  }
+
+  /**
+   * 处理预览图
+   */
+  static _processGoodsPreview(item) {
+    const images = item.images;
+    // 图片处理
+    if (images == null || images.length < 1) {
+      item.imageUrl = '/images/icons/broken.png"'
+    }
+    else if (images[0].url == null) {
+      item.imageUrl = '/images/icons/broken.png';
+    }
+    else {
+      item.imageUrl = images[0].url;
+    }
+  }
+
+  /**
+   * 处理商品区间
+   */
+  static _processGoodsPriceRange(detail) {
+    if (!detail.goodsSkuInfo || !detail.goodsSkuInfo.goodsSkuDetails) {
+      const price = parseFloat(detail.sellPrice).toFixed(2);
+      detail.priceText = `￥${price}`;
+      return;
+    }
+    const skuDetails = detail.goodsSkuInfo.goodsSkuDetails;
+    let maxPrice = 0;
+    let minPrice = 0;
+
+    for (let i in skuDetails) {
+      const detail = skuDetails[i].goodsSkuDetailBase;
+      maxPrice = Math.max(detail.price, maxPrice).toFixed(2);
+      minPrice = Math.min(detail.price, maxPrice).toFixed(2);
+    }
+    detail.maxPrice = maxPrice;
+    detail.minPrice = minPrice;
+    if (maxPrice != minPrice) {
+      detail.priceText = `￥${minPrice} ~ ${maxPrice}`;
+    } else {
+      detail.priceText = `￥${minPrice}`;
+    }
   }
 }
