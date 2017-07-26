2c40d3627dcf63b607510d5f803f300183040866
coolhwm <coolhwm@qq.com>
2017-07-26 18:01:59 +0800

    完成商品评价

---
+++ src/api/comment.js
@@ -0,0 +1,46 @@
+
+import base from './base';
+import Page from '../utils/Page';
+
+export default class comment extends base {
+  /**
+   * 分页方法
+   */
+  static page () {
+    const url = `${this.baseUrl}/comments`;
+    return new Page(url, this._processGoodsComment.bind(this));
+  }
+
+  /**
+   * 创建
+   */
+  static async create(comments) {
+    const url = `${this.baseUrl}/comments`;
+    return await this.post(url, comments);
+  }
+
+  /**
+   * 评价统计
+   */
+  static async count(goodsId) {
+    const url = `${this.baseUrl}/comments/count?goods_id=${goodsId}`;
+    return await this.get(url);
+  }
+
+  /**
+   * 处理评价列表数据
+   */
+  static _processGoodsComment(data) {
+    const comment = {};
+    comment.createTime = data.createTime.substring(0, 10);
+    comment.starArr = [0, 0, 0, 0, 0];
+    for (let i = 0; i < data.star; i++) {
+      comment.starArr[i] = 1;
+    }
+    comment.star = data.star;
+    comment.avatar = data.customer.avatarUrl;
+    comment.nickName = data.customer.nickName;
+    comment.comment = data.comment;
+    return comment;
+  }
+}
