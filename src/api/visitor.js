4e97c22fee1f8d65e428f6513dd6b9917880d7eb
mmmmg <2291918627@qq.com>
2017-10-21 11:21:04 +0800

    ADD:完成访客详情页面

--- src/api/visitor.js
+++ src/api/visitor.js
@@ -1,42 +1,39 @@
-import base from './base'
-import Page from '../utils/Page'
+import base from './base';
+import Page from '../utils/Page';
 
 export default class visitor extends base {
   /**
    * 分页方法
    */
   static page () {
-    const url = `${this.baseUrl}/customers/visit_list`
-    return new Page(url, this._processShopsVisitor.bind(this))
+    const url = `${this.baseUrl}/customers/visit_list`;
+    return new Page(url, this._processShopsVisitor.bind(this));
   }
 
   /**
    * 创建
    */
   static async create (comments) {
-    const url = `${this.baseUrl}/customers/visit_list`
-    return await this.post(url, comments)
+    const url = `${this.baseUrl}/customers/visit_list`;
+    return await this.post(url, comments);
   }
 
   /**
    * 浏览统计
    */
   static async count (goodsId) {
-    const url = `${this.baseUrl}/count/visit_count`
-    return await this.get(url)
+    const url = `${this.baseUrl}/count/visit_count`;
+    return await this.get(url);
   }
 
   /**
    * 处理浏览数列表数据
    */
   static _processShopsVisitor (data) {
-    console.log(data)
-    const visitor = {}
-    // visitor.createTime = data.createTime.substring(0, 10);
-    // visitor.avatar = data.avatarUrl;
-    visitor.name = data.nickName
-    visitor.key = '访问时间'
-    visitor.value = 0
-    return visitor
+    const visitor = {};
+    visitor.name = data.nickName;
+    visitor.key = '访问时间';
+    visitor.value =data.loginTime.substring(0, 16);
+    return visitor;
   }
 }
